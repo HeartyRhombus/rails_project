3b8be7c5adb383851a369eddd61c623bede945d3
HeartyRhombus <kaylahmg@gmail.com>
2020-11-30 16:19:35 -0800

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
 end
