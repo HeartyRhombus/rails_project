046f8a340ac18eacbd85a15e783453b70df23178
HeartyRhombus <kaylahmg@gmail.com>
2020-11-30 16:19:53 -0800

    add registrations route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root 'application#welcome'
 
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
