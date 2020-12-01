5b7cfcb802a863ec2d7d02f3f870e6e42132eeaf
HeartyRhombus <kaylahmg@gmail.com>
2020-12-01 14:33:33 -0800

    add new character form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,37 @@
 class CharactersController < ApplicationController
-    belongs_to :user
-    belongs_to :class
-    belongs_to :race
-    
+
+    def index
+        @characters = current_user.characters.all
+    end
+
+    def new
+        @character = current_user.characters.build
+
+        # @character.race_id = 3
+        # @character.race.build
+        # @character.career.build
+    end
+
+    def create
+        binding.pry
+
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    def character_params
+        params.require(:character).permit(:name, :level, :race_id, :career_id, :user_id)
+    end
 end
