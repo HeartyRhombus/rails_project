b5f60ce17582e932a3bddd76bf0074a511e21301
HeartyRhombus <kaylahmg@gmail.com>
2020-12-02 14:18:12 -0800

    update to run if applicable params

--- app/helpers/careers_helper.rb
+++ app/helpers/careers_helper.rb
@@ -1,2 +1,9 @@
 module CareersHelper
+
+    def career_name(career_id)
+        if params[:career_id]
+            career = Career.find_by(id: params[:career_id])
+            career.name
+        end
+    end
 end
--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,2 +1,10 @@
 module RacesHelper
+
+    def race_name(race_id)
+        if params[:race_id]
+            race = Race.find_by(id: params[:race_id])
+            race.name
+        end
+    end
+
 end
