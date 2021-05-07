732ccab196217fb80a8d95d8b69c2928210ebb9d
HeartyRhombus <kaylahmg@gmail.com>
2021-05-06 17:02:58 -0700

    add exclusion to validation to allow race w/ diff factions

--- app/models/race.rb
+++ app/models/race.rb
@@ -3,7 +3,7 @@ class Race < ApplicationRecord
     has_many :characters
     has_many :careers, through: :characters
 
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true, exclusion: {in: %w(Pandaren)}
     validates :faction, inclusion: {in: %w(Alliance Horde)}
 
 end
